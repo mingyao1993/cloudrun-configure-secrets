6ed01c9d138154d0ba07b9269e599629fd4b5f4c
Michael Loh <michael.loh@virginmediao2.co.uk>
2024-03-11 21:04:51 +0000

    refactor code

--- main.py
+++ main.py
@@ -7,17 +7,17 @@ from google.cloud import secretmanager
 app = FastAPI(title="gcp-cloudrun-access-secret-manager")
 
 
-def get_secret_from_env_var() -> None:
-    print(f"Secret value from env var: {os.getenv('my_secret')}")
+def get_secret_from_env_var() -> str:
+    return os.getenv('my_secret')
 
 
-def get_secret_from_volume_mount() -> None:
+def get_secret_from_volume_mount() -> str:
     with open("/secrets/my_secret", "r") as file:
         file_contents = file.read()
-    print(f"Secret value from volume mount: {file_contents}")
+    return file_contents
 
 
-def get_secret_from_api() -> None:
+def get_secret_from_api() -> str:
     project_id = os.getenv("PROJECT_ID")
     secret_id = "my_secret"
     version_id = "latest"
@@ -32,15 +32,18 @@ def get_secret_from_api() -> None:
     response = client.access_secret_version(request={"name": name})
     payload = response.payload.data.decode("UTF-8")
 
-    print(f"Secret value from API: {payload}")
+    return payload
 
 
 @app.post("/get_my_secret", status_code=200)
 def get_my_secret(request: Request):
-    get_secret_from_env_var()
+    env_var_secret = get_secret_from_env_var()
+    print(f"Secret value from env var: {env_var_secret}")
 
-    get_secret_from_volume_mount()
+    volume_mount_secret = get_secret_from_volume_mount()
+    print(f"Secret value from volume mount: {volume_mount_secret}")
 
-    get_secret_from_api()
+    api_secret = get_secret_from_api()
+    print(f"Secret value from API: {api_secret}")
 
     return {"status_code": 200}
